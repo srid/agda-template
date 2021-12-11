b4691b69cc50c988569dfd5363970b46cecbd271
Sridhar Ratnakumar <srid@srid.ca>
2021-12-11 18:22:12 -0500

    add shell.nix

--- flake.nix
+++ flake.nix
@@ -1,17 +1,23 @@
 {
   description = "Agda project";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem
       (system:
         let
           pkgs = import nixpkgs { inherit system; };
-          agdaPkgs = 
-            if system == "aarch64-darwin" 
-              then import nixpkgs { system = "x86_64-darwin"; }  # Rosetta (Agda broken on M1)
-              else pkgs;
+          agdaPkgs =
+            if system == "aarch64-darwin"
+            then import nixpkgs { system = "x86_64-darwin"; }  # Rosetta (Agda broken on M1)
+            else pkgs;
         in
         {
           devShell = pkgs.mkShell {
